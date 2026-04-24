e05d97de50e402c37bb89ab1b630483273b7e793
Kushal Patankar <kushal@zoca.ai>
2026-04-24 18:13:22 +0530

    chore: add gsap animation adapter

---
+++ src/animation/animator.js
@@ -0,0 +1,23 @@
+import { gsap } from 'gsap'
+
+export const animator = {
+  to(target, vars) {
+    return gsap.to(target, vars)
+  },
+
+  fromTo(target, fromVars, toVars) {
+    return gsap.fromTo(target, fromVars, toVars)
+  },
+
+  timeline(vars) {
+    return gsap.timeline(vars)
+  },
+
+  killTweensOf(target, properties) {
+    return gsap.killTweensOf(target, properties)
+  },
+
+  ticker: gsap.ticker,
+}
+
+export { gsap }
